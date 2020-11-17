53b9dc2cc4a86bb693aeac052e3aecef19a1826c
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-11-17 16:51:52 -0500

    trying to not whoops

--- src/main/java/com/tim/gotthere_server/WebSocketConfig.java
+++ src/main/java/com/tim/gotthere_server/WebSocketConfig.java
@@ -2,14 +2,17 @@ package com.tim.gotthere_server;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
+import org.springframework.session.ExpiringSession;
 
 @Configuration
+@EnableScheduling
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-	
+public class WebSocketConfig<S extends ExpiringSession> extends AbstractSessionWebSocketMessageBrokerConfigurer<S> {
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/topic");
@@ -17,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	}
 
 	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
+	protected void configureStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/gs-guide-websocket").withSockJS();
 	}
 }
